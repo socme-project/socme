40f872c60d8a196683353593688b43ddc43087bc
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-03-11 11:45:28 +0100

    up

--- backend/notifyme/cmd/main.go
+++ backend/notifyme/cmd/main.go
@@ -10,9 +10,22 @@ var telegramToken = "7640444898:AAGas29ZrF6vV8Wx8iSbyF-rNlR_QZdJShY"
 func main() {
 	tel := notifyme.Telegram(0, telegramToken)
 
-	test, err := tel.HelperGetChatId()
-	fmt.Println(test, err)
+	test, err := tel.HelperGetChatIds()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	tel = notifyme.Telegram(test, telegramToken)
-	tel.Notify("Hello from Go!")
+	chatid := test["hadrienaka"]
+	if chatid == 0 {
+		fmt.Println("ChatId not found")
+	}
+
+	tel = notifyme.Telegram(chatid, telegramToken)
+
+	notifiers := notifyme.Notifiers{tel}
+
+	errs := notifiers.Notify("Test", "Hello World")
+	if errs != nil {
+		fmt.Println(errs)
+	}
 }
--- backend/notifyme/notifyme.go
+++ backend/notifyme/notifyme.go
@@ -1,68 +1,17 @@
 package notifyme
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-)
-
-type telegram struct {
-	ChatId int
-	Token  string
+type Notifier interface {
+	Notify(title, message string) error
 }
 
-func Telegram(chatId int, token string) *telegram {
-	return &telegram{ChatId: chatId, Token: token}
-}
+type Notifiers []Notifier
 
-func (t *telegram) Notify(message string) error {
-	chatId := fmt.Sprintf("%d", t.ChatId)
-	response, err := http.Post(
-		"https://api.telegram.org/bot"+t.Token+"/sendMessage",
-		"application/json",
-		bytes.NewBufferString(`{"chat_id": "`+chatId+`", "text": "`+message+`"}`),
-	)
-	if err != nil {
-		return err
-	}
-	if response.StatusCode != http.StatusOK {
-		return err
-	}
-	return nil
-}
-
-func (t *telegram) HelperGetChatId() (int, error) {
-	// BOT_TOKEN="YOURBOTTOKEN" curl -s "https://api.telegram.org/bot$BOT_TOKEN/getUpdates" | grep -o '"id":[0-9]*' | head -1 | cut -d':' -f2
-	response, err := http.Get("https://api.telegram.org/bot" + t.Token + "/getUpdates")
-	if err != nil {
-		return 0, err
-	}
-	defer response.Body.Close()
-
-	type responseType struct {
-		Result []struct {
-			Message struct {
-				Chat struct {
-					Id int `json:"id"`
-				}
-			}
+func (n Notifiers) Notify(title, message string) []error {
+	var errs []error = nil
+	for _, notifier := range n {
+		if err := notifier.Notify(title, message); err != nil {
+			errs = append(errs, err)
 		}
 	}
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	var result responseType
-	if err := json.Unmarshal(body, &result); err != nil {
-		return 0, err
-	}
-	if len(result.Result) == 0 {
-		return 0, fmt.Errorf("No chat ID found")
-	}
-
-	return responseType(result).Result[0].Message.Chat.Id, nil
+	return errs
 }
