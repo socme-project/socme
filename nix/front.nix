16656334945d4b47d9abeed2d36a9dd55b9a41a8
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-07-04 17:43:14 +0200

    front nix done
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

---
+++ nix/front.nix
@@ -0,0 +1,37 @@
+{ pkgs, lib, bun2nix }:
+
+let
+  packageJson = lib.importJSON ../front/package.json;
+
+  pname = if lib.isAttrs packageJson && lib.hasAttr "name" packageJson then
+    packageJson.name
+  else
+    "socme-frontend-default";
+
+  version = if lib.isAttrs packageJson && lib.hasAttr "version" packageJson then
+    packageJson.version
+  else
+    "0.0.0";
+
+in bun2nix.lib.${pkgs.system}.mkBunDerivation {
+  inherit pname version;
+  src = ../front;
+
+  bunNix = ../front/bun.nix;
+
+  index = null;
+
+  nativeBuildInputs = [ pkgs.bun pkgs.vite pkgs.typescript pkgs.rsync ];
+
+  buildPhase = ''
+    echo "Running bun build for static assets..."
+    export QT_QPA_PLATFORM=offscreen
+    ${pkgs.bun}/bin/bun run build
+  '';
+
+  installPhase = ''
+    echo "Installing frontend artifacts..."
+    mkdir -p $out/share/${pname}
+    cp -r dist/* $out/share/${pname}/
+  '';
+}
