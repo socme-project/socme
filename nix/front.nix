a4253d7f78765a884c76aa92d4ced619176ae308
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-07-04 18:03:57 +0200

    removed debugging
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- nix/front.nix
+++ nix/front.nix
@@ -18,7 +18,7 @@ bun2nix.lib.${pkgs.system}.mkBunDerivation {
 
   bunNix = ../front/bun.nix;
 
-  outputHash = "sha256-yeIqS+QKMwMrdb3SAhNKoINBVE8AjFAXNAIoEDZZ/GI="; 
+  outputHash = "sha256-+yeCM2fhdD/rtrG6ZeRY4a5+OgjGiIUtLtgoBlfE9zY="; 
   outputHashAlgo = "sha256";
   outputHashMode = "recursive"; 
 
@@ -27,31 +27,12 @@ bun2nix.lib.${pkgs.system}.mkBunDerivation {
   nativeBuildInputs = [ pkgs.bun pkgs.vite pkgs.typescript pkgs.rsync ];
 
   buildPhase = ''
-
-    echo "--- Contents of current directory before build: ---"
-    ls -lA .
-    echo "---------------------------------------------------"
-
     ${pkgs.bun}/bin/bun run build
-
-    echo "--- Contents of 'dist' directory after build: ---"
-    ls -lA dist/ || echo "dist directory not found or empty after build!"
-    echo "---------------------------------------------------"
   '';
 
   installPhase = ''
-    echo "Installing frontend artifacts..."
     mkdir -p $out/share/${pname}
-
-    echo "--- Contents of 'dist' directory before copy in installPhase: ---"
-    ls -lA dist/ || echo "dist directory not found or empty before copy!"
-    echo "----------------------------------------------------------------"
-
     cp -r dist/* $out/share/${pname}/
-
-    echo "--- Contents of $out/share/${pname}/ after copy: ---"
-    ls -lA $out/share/${pname}/ || echo "Output directory not found or empty after copy!"
-    echo "----------------------------------------------------"
   '';
 }
 
